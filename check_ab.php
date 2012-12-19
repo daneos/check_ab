d20808fbe68c26ed88e6337fd7a2e44e79b37866
daneos <daneos@daneos.com>
2012-12-19 14:31:18 +0100

    first sketch

--- check_ab.php
+++ check_ab.php
@@ -6,6 +6,12 @@
 	$STATE_CRITICAL=2;
 	$STATE_UNKNOWN=3;
 
+	$url = FALSE;
+	$file = FALSE;
+	$reqcnt = explode(':', "1000:100");
+	$percent = explode(':', "5:10");
+	$db = explode(':', "86400:AVERAGE");
+
 	function usage()
 	{
 		echo "Usage: ".$argv[0]." <options>\n";
@@ -13,12 +19,14 @@
 		echo "Options:\n";
 		echo "-u URL (required)\n";
 		echo "-f RRD file (required)\n";
-		echo "-n (x:y) x-request count y-one time request count (defaults to 1:1)"
+		echo "-n (x:y) x-request count y-one time request count (defaults to 1000:100)";
+		echo "-p (x:y) x-percent for CRIT y-percent for WARN (defaults to 5:10)";
+		echo "-d (x:y) RRD database to check x-seconds y-name (defaults to (86400:AVERAGE)"
 		echo "-h shows this help message\n";
-		echo "Example: ".$argv[0]." -u http://example.com/ -f test.rrd -n 10000:100\n";
+		echo "Example: ".$argv[0]." -u http://example.com/ -f test.rrd -n 10000:100 -p 10:20 -d 300:MAX\n";
 	}
 
-	$options = getopt("hf:t:n:");
+	$options = getopt("hu:f:n:p:d:");
 	foreach($options as $option=>$val)
 	{
 		switch($option)
@@ -31,6 +39,12 @@
 				break;
 			case 'n':
 				$reqcnt = explode(':', $val);
+				break;
+			case 'p':
+				$percent = explode(':', $val);
+				break;
+			case 'd':
+				$db = explode(':', $val);
 				break;
 			case 'h':
 				usage();
@@ -54,7 +68,47 @@
 		echo "No filename specified.\n";
 		return 1;
 	}
-	if(!$reqcnt)
+
+	exec("ab -q -n ".escapeshellarg($reqcnt[0])." -c ".escapeshellarg($reqcnt[1])." ".escapeshellarg($url), $ab);
+	$ab_out = implode('', $ab);
+	exec("rrdtool dump ".escapeshellarg($file), $rrd);
+	$rrdxml = simplexml_load_string(implode('', $rrd));
+
+	$regexp="~Requests per second\:.*[0-9\.]+~";
+	for($i=0; $i < count($ab); $i++)
 	{
-		$reqcnt = explode(':', "1:1")
+		if(preg_match($regexp, $ab[$i], $match))
+		{
+			$exploded = explode(':', $match[0]);
+			$rps = trim($exploded[1]);
+			break;
+		}
 	}
+	echo "$rps\n";
+
+	for($i=0; $i < count($rrdxml->rra); $i++)
+	{
+		if($rrdxml->rra[$i]->pdp_per_row == $db[0]/$rrdxml->step)
+		{
+			if($rrdxml->rra[$i]->cf == $db[1])
+			{
+				for($j=0; $j < count($rrdxml->rra[$i]->database->row); $j++)
+				{
+					$reqperc = 100 - (($rrdxml->rra[$i]->database->row[$j]->v / $rps)*100);
+					if($reqperc < $percent[0])
+					{
+						echo "CRITICAL: Server can handle only ".$reqperc." more requests\n";
+						return STATE_CRITICAL;
+					}
+					if($reqperc < $percent[1])
+					{
+						echo "WARNING: Server can handle only ".$reqperc." more requests\n";
+						return STATE_WARNING;
+					}
+
+				}
+			}
+		}
+	}
+	echo "OK: Requests per second: ".$rps."\n";
+	return STATE_OK;
